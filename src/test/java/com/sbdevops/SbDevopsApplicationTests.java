9af8e2400099f5506ad60fbed467f0070c57c012
Kanhav Mathur <kanhavmathur05@gmail.com>
2021-04-29 15:54:39 +0530

    Second Commit

--- src/test/java/com/sbdevops/SbDevopsApplicationTests.java
+++ src/test/java/com/sbdevops/SbDevopsApplicationTests.java
@@ -1,6 +1,8 @@
 package com.sbdevops;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.stream.Collectors;
@@ -36,9 +38,16 @@ class SbDevopsApplicationTests {
 	}
 	
 	public void saveUserTest() {
-		User user=new User(999,"pranya",33,"Pune");
+		User user=new User(999,"Pranya",33,"Pune");
 		when(repository.save(user)).thenReturn(user);
 		assertEquals(user,service.addUser(user));
+	}
+	
+	@Test
+	public void deleteUserTest() {
+		User user=new User(999,"Pranya",33,"Pune");
+		service.deleteUser(user);
+		verify(repository,times(1)).delete(user);
 	}
 	
 	@Test
